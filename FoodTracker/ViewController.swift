49439191ecddee3b26ea7683373593806ef16b44
Mark Tareshawty <mark@marktareshawty.com>
2015-11-29 16:58:17 -0500

    Second checkpoint successful
    Checkpoint: Test your changes by running Simulator. When you click the
    Set Default Label Text button, the label should change from `Meal Name`
    (the value set in your storyboard) to `Default Text` (the value set by the
    action).

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  // MARK: Properties
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var mealNameLabel: UILabel!
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,10 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  // MARK: actions
+  @IBAction func setDefaultTextLabel(sender: UIButton) {
+    mealNameLabel.text = "Default Text"
+  }
 
 }
 
