5bd25af0e725479fe272a89cd302f6f0437c0e1b
Mark Tareshawty <mark@marktareshawty.com>
2015-11-29 21:54:53 -0500

    all tests pass

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -11,18 +11,20 @@ import XCTest
 @testable import FoodTracker
 
 class FoodTrackerTests: XCTestCase {
-    
+
   // MARK: FoodTracker Tests
+
+  // Tests to confirm that the Meal initializer returns when no name or a negative rating is provided.
   func testMealInitialization() {
     // Success case.
     let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)
     XCTAssertNotNil(potentialItem)
-    
+
     // Failure cases.
     let noName = Meal(name: "", photo: nil, rating: 0)
     XCTAssertNil(noName, "Empty name is invalid")
-    
+
     let badRating = Meal(name: "Really bad rating", photo: nil, rating: -1)
-    XCTAssertNotNil(badRating)
+    XCTAssertNil(badRating, "Negative ratings are invalid, be positive")
   }
 }
