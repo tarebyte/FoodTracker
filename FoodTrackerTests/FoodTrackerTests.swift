d87cf578b7405ad608b45e8455559a013500424a
Mark Tareshawty <mark@marktareshawty.com>
2015-11-29 21:50:22 -0500

    tests run, two pass one fail

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -6,31 +6,23 @@
 //  Copyright © 2015 Mark Tareshawty. All rights reserved.
 //
 
+import UIKit
 import XCTest
 @testable import FoodTracker
 
 class FoodTrackerTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  // MARK: FoodTracker Tests
+  func testMealInitialization() {
+    // Success case.
+    let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)
+    XCTAssertNotNil(potentialItem)
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+    // Failure cases.
+    let noName = Meal(name: "", photo: nil, rating: 0)
+    XCTAssertNil(noName, "Empty name is invalid")
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+    let badRating = Meal(name: "Really bad rating", photo: nil, rating: -1)
+    XCTAssertNotNil(badRating)
+  }
 }
