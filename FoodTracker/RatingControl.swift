dcb38f37894a875526055d076a5952ea549d4370
Mark Tareshawty <mark@marktareshawty.com>
2015-11-29 20:50:30 -0500

    Eighth checkpoint successful
    Run your app. You should see stars instead of red buttons. Clicking any
    of the buttons at this point should still call `ratingButtonTapped(_:)`
    and log the message to the console, but your buttons don’t change images
    yet. You’ll fix that next.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -11,17 +11,27 @@ import UIKit
 class RatingControl: UIView {
 
   // MARK: Properties
-  var rating = 0
+  var rating        = 0
   var ratingButtons = [UIButton]()
+  var spacing       = 5
+  var stars         = 5
 
   // MARK: Initialization
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
 
-    for _ in 0..<5 {
-      let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+    let emptyStarImage = UIImage(named: "emptyStar")
+    let filledStarImage = UIImage(named: "filledStar")
 
-      button.backgroundColor = UIColor.redColor()
+    for _ in 0..<5 {
+      let button = UIButton()
+
+      button.setImage(emptyStarImage, forState: .Normal)
+      button.setImage(filledStarImage, forState: .Selected)
+      button.setImage(filledStarImage, forState: [.Highlighted, .Selected])
+
+      button.adjustsImageWhenHighlighted = false
+
       button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
 
       ratingButtons += [button]
@@ -30,8 +40,23 @@ class RatingControl: UIView {
     }
   }
 
+  override func layoutSubviews() {
+    // Set the button's width and height to a square the size of the frame's height.
+    let buttonSize = Int(frame.size.height)
+    var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+
+    // Offset each button's origin by the length of the button plus some spacing.
+    for (index, button) in ratingButtons.enumerate() {
+      buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
+      button.frame = buttonFrame
+    }
+  }
+
   override func intrinsicContentSize() -> CGSize {
-    return CGSize(width: 240, height: 44)
+    let buttonSize = Int(frame.size.height)
+    let width = (buttonSize + spacing) * stars
+
+    return CGSize(width: width, height: buttonSize)
   }
 
   // MARK: Button Action
