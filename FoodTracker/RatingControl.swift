a3139cf5957d7e74c82ec5b119968aa07d45cb75
Mark Tareshawty <mark@marktareshawty.com>
2015-11-29 21:01:47 -0500

    Nineth checkpoint successful
    Run your app. You should see five stars and be able to click one to
    change the rating. Click the third star to change the rating to 3, for
    example.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -11,7 +11,12 @@ import UIKit
 class RatingControl: UIView {
 
   // MARK: Properties
-  var rating        = 0
+  var rating = 0 {
+    didSet {
+      setNeedsLayout()
+    }
+  }
+
   var ratingButtons = [UIButton]()
   var spacing       = 5
   var stars         = 5
@@ -50,6 +55,8 @@ class RatingControl: UIView {
       buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
       button.frame = buttonFrame
     }
+
+    updateButtonSelectionStates()
   }
 
   override func intrinsicContentSize() -> CGSize {
@@ -61,6 +68,15 @@ class RatingControl: UIView {
 
   // MARK: Button Action
   func ratingButtonTapped(button: UIButton) {
-    print("Button pressed!")
+    rating = ratingButtons.indexOf(button)! + 1
+
+    updateButtonSelectionStates()
+  }
+
+  func updateButtonSelectionStates() {
+    for (index, button) in ratingButtons.enumerate() {
+      // If the index of a button is less than the rating, that button shouldn't be selected.
+      button.selected = index < rating
+    }
   }
 }
